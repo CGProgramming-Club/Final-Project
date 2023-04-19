45e599a8fe2bbd68f847a8041c974350378a971a
gingerjakedaboi <gingerjakedaboi>
2023-04-19 15:45:04 -0400

    fix code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
-#include "iostream"
+#include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Welcome to the Space Casino!" << endl;
+
     return 0;
 }
