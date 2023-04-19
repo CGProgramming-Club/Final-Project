5ba440d3bf53e8948a88a7db91b005384fe8dff2
gingerjakedaboi <gingerjakedaboi>
2023-04-19 15:57:55 -0400

    make an array and count it

--- main.cpp
+++ main.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int main() {
-    cout << "Welcome to the Space Casino!" << endl;
+    string personalIdentification;
+    string games[] = {"Roulette", "Blackjack", "Slots", "Craps"};
+
+    cout << "Welcome to the Space Casino!\nWhat is your personal identification?\n" << endl;
+
+    cin >> personalIdentification;
+    cout << "Hello, " + personalIdentification + "\n\n";
+
+    for(int i = 0; i < sizeof(games) / sizeof(string); i++) {
+        cout << i;
+    }
 
     return 0;
 }
